7a928b82bb8012b6e136ea0044f3f0ed17ae8e13
DaniloBarros <dan.b412@gmail.com>
2014-09-21 23:29:17 -0300

    Tried to code something that actually work

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
 module Main where
 
 import System.IO
-import Agenda
+import Agenda (toPerson, insertPerson, deleteFromPersons)
 
 main :: IO()
 main = do
+	hSetBuffering stdin NoBuffering
 	putStr("\n\tMenu:")
 	putStr("\n1-Adicionar Contato")
 	putStr("\n2-Deletar Contato")
@@ -15,6 +16,7 @@ main = do
 	putStr("\n7-Exportar Tudo")
 	putStr("\n8-Importar Arquivo Contatos")
 	putStr("\n9-Importar Arquivo Lembretes")
+	putStr("\n0-Sair")
 	putStr("\n\nDigite a opcao: ")
 	
 	c<-getChar
@@ -23,14 +25,41 @@ main = do
 
 	where action c
 		|c=='1' = do
-			putStr("\nDigite o nome do contato")
-			n<-getLine
-			putStr("\nDigite o numero do contato")
-			t <- getLine
-			insertAgenda agenda n t
+			putStr("\nDigite o nome do contato: ")
+
+			name <- getLine
+			{-Nao faco ideia do porque, mas so com um
+			getLine ele pula direto para o telephone-}
+			name <- getLine
+			putStr("\nDigite o numero do contato: ")
+			tel <- getLine
+			
+			let person = toPerson name tel
+			let agenda = insertPerson agenda person
+
+			putStr ("Contato adicionado " ++ show name++"\n")
+			main	
+
+		|c=='2' = do
+			putStr("\nDigite o nome do contato a ser deletado")
+			
+			name <- getLine
+			name <- getLine
+			putStr("\nDigite o numero do contato: ")
+			tel <- getLine
+			
+			let person = toPerson name tel
+			putStr("\nDigite o numero do contato: ")
 		{-|c=='8' = do
 			putStr("\nDigite o nome do arquivo")
 			file <- getLine
 			inpCont <- readFile file 
 			contacts = parserContacts inpCont
 			-}
+
+		|c=='5' = do
+			writeFile "OutContacts.txt" agenda
+			main
+
+		|c=='0' = do
+			putStr("Tchau...\n")
